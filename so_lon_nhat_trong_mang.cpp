8311451c8f68ced89d69d22d4b2f33cecf1a3aa8
TVS0n <109617105+TVS0n@users.noreply.github.com>
2022-08-01 20:19:46 +0700

    bai1buoi12

---
+++ so_lon_nhat_trong_mang.cpp
@@ -0,0 +1,39 @@
+# include <stdio.h>
+int main ()
+{
+
+	int n;
+	printf("nhap n= ");
+	scanf("%d",&n);
+
+
+	int arr[n];
+	int i,high,m;
+
+	
+	
+	for (int i =0;i<n;i++)// duyt mang
+	{
+			printf("\nnhap  so nguyen %d ",i+1);	
+		scanf("%d",&arr[i]);
+	
+	}
+	// hien thi gia tri lon nhat 
+		high=arr[0];
+	
+	for (int i=1;i<n;i++){
+		
+		if (arr[i]>high)
+		{
+			m=high;
+			high=arr[i];
+		} if (arr[i] >m && arr[i] < high)
+		{
+			m=arr[i];
+		}
+		
+	}
+	
+	printf("\n gia tri max %d",high);
+	printf("\n gia tri gan lon nha %d",m);
+}
